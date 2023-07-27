6b0177699c0b51f926308f9ee7e6e4e304ece7a8
usma11dia0 <usma11dia0@gmail.com>
2023-07-27 23:18:56 +0900

    reset server.py and client.py

--- old/client.py
+++ old/client.py
@@ -1,23 +1,10 @@
+# Echo client program
 import socket
 
-# サーバーの設定
-server_ip = '127.0.0.1'
-server_port = 12345
-
-# ソケットを作成
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# サーバーに接続
-client_socket.connect((server_ip, server_port))
-print('サーバーに接続しました')
-
-# サーバーにデータを送信する
-client_socket.sendall('Hello, Server!'.encode())
-
-# サーバーからのデータを受信する
-data = client_socket.recv(1024)
-print(f'受信データ: {data.decode()}')
-
-# ソケットを閉じる
-client_socket.close()
-print(f'clientのソケットを閉じました。')
+HOST = 'localhost'    # The remote host
+PORT = 50007              # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
--- old/server.py
+++ old/server.py
@@ -1,34 +1,15 @@
-# 公式ドキュメント socketモジュール
-# https://docs.python.org/ja/3/library/socket.html
-
+# Echo server program
 import socket
 
-# サーバーの設定
-server_ip = '127.0.0.1'
-server_port = 12345
-
-# ソケットを作成
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# ソケットのバインド
-server_sock.bind((server_ip, server_port))
-
-# ソケットのリッスン
-server_sock.listen(1)
-print('サーバーが起動しました。')
-
-# クライアントからの接続を待つ
-client_sock, client_address = server_sock.accept()
-print(f'{client_address}から接続しました。')
-
-# クライアントからのデータを受信する
-data = client_sock.recv(1024)
-print(f'クライアントからのデータ: {data.decode()}')
-
-# クライアントにデータを送信する
-client_sock.sendall('Hello, Client!'.encode())
-
-# ソケットを閉じる
-client_sock.close()
-server_sock.close()
-print('サーバーを停止しました。')
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(10)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
