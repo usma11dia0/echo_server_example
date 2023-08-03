a50605f896bb9704592d3d6d02154ac2097f35cd
usma11dia0 <usma11dia0@gmail.com>
2023-08-04 00:59:19 +0900

    add new client_server.py

--- client.py
+++ client.py
@@ -3,8 +3,18 @@ import socket
 HOST = 'localhost'
 PORT = 12345
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-sock.sendall(b'Hello world')
-# data = sock.recv(1024)
-# print(data)
+socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket2.connect((HOST, PORT))
+socket2.sendall(b'Hello World')
+
+data = socket2.recv(1024)
+print(data)
+
+
+#リファクタリング後
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello World')
+    data = s.recv(1024)
+print(data)
--- server.py
+++ server.py
@@ -3,21 +3,36 @@ import socket
 HOST = 'localhost'
 PORT = 12345
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
+socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket1.bind((HOST, PORT))
 
-sock.listen(1)
-conn, address = sock.accept()
+socket1.listen(1)
+(conn, address) = socket1.accept()
 
-print(f'sock:{sock}')
-print(f'conn:{conn}')
-print(f'address:{address}')
+print(f'socket1: {socket1}')
+print(f'conn: {conn}')
+print(f'address: {address}')
 
 while True:
-    pass
-    # data = conn.recv(1024)
-    # print(f'data: {data}')
-    # # conn.sendall(data)
-    # # if not data:
-    # #     break
+    data = conn.recv(1024)
+    print(data)
+    if not data:
+        break
+    conn.sendall(data)
+
+socket1.close()
+
+# リファクタリング後
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, address = s.accept()
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            if not data:break
+            conn.sendall(data)
+
+
 
