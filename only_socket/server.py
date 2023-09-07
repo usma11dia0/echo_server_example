2aee63f7cc6881e91d15d8de16dd8b0812ff5c99
usma11dia0 <usma11dia0@gmail.com>
2023-09-07 22:46:19 +0900

    modified: add example of without-refactoring to only_socket

--- only_socket/server.py
+++ only_socket/server.py
@@ -1,28 +1,3 @@
-import socket
-
-HOST = 'localhost'
-PORT = 12345
-
-socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket1.bind((HOST, PORT))
-
-socket1.listen(1)
-(conn, address) = socket1.accept()
-
-print(f'socket1: {socket1}')
-print(f'conn: {conn}')
-print(f'address: {address}')
-
-while True:
-    data = conn.recv(1024)
-    print(data)
-    if not data:
-        break
-    conn.sendall(data)
-
-socket1.close()
-
-# リファクタリング後
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
@@ -33,6 +8,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(data)
             if not data:break
             conn.sendall(data)
+
+# リファクタリング前
+# import socket
+
+# HOST = 'localhost'
+# PORT = 12345
+
+# socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# socket1.bind((HOST, PORT))
+
+# socket1.listen(1)
+# (conn, address) = socket1.accept()
+
+# print(f'socket1: {socket1}')
+# print(f'conn: {conn}')
+# print(f'address: {address}')
+
+# while True:
+#     data = conn.recv(1024)
+#     print(data)
+#     if not data:
+#         break
+#     conn.sendall(data)
+
+# socket1.close()
 
 
 
